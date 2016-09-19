0c5eed66e77e0e4f14efc6e1e9d1c71b7bec0feb
ericslc <eric.leatham@gmail.com>
2016-09-19 16:30:46 -0600

    updated states

--- angular-template/js/app.js
+++ angular-template/js/app.js
@@ -5,13 +5,15 @@ angular.module('myApp').config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
       .state('home', {
          url: '/',
-         templateUrl: '/home.html',
+         templateUrl: './views/home.html',
          controller: 'mainController'
+         access: {restricted: false}
       })
       .state('blog', {
         url: '/blog',
-        templateUrl: '/blog.html',
+        templateUrl: './views/blog.html',
         controller: 'mainController'
+        access: {restricted: false}
       })
       $urlRouterProvider.otherwise('/')
 })
